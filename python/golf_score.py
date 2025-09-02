d8568ca4188b5a6a381d9f5b6df6d6e274d8b761
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-09-02 10:12:18 +0900

    ゴルフスコアやり直し一回め

--- python/golf_score.py
+++ python/golf_score.py
@@ -8,65 +8,53 @@
 
 row1 = []
 row2 = []
-
 score = []
 
 # ファイルを読み取りモードで開く
 with open('case1.txt', 'r') as f:
     lines = f.readlines()  # 全行をリストで取得
-
-
     row1 = lines[0].strip().split(',') 
     row2 = lines[1].strip().split(',') 
 
-    
 print("row1:", row1)
 print("row2:", row2)
 
 
-def hantei (kitei_row, dasuu_row):
+def hantei (par_row, score_row):
+
+    SCORE_MAPPING = {
+    -4 : "コンドル",
+    -3 : "アルバトロス",
+    -2 : "イーグル",
+    -1 : "バーディ",
+    0 : "パー",
+    1 : "ボギー",
+    2 : "2ボギー",
+    3 : "3ボギー",
+    4 : "4ボギー"
+    }
 
     result = None  
+    par = int(par_row)
+    score = int(score_row)
+    difference = score - par
 
-    kitei = int(kitei_row)
-    dasuu = int(dasuu_row)
-
-    if kitei == dasuu:
-        result  = "パー"
-    
-    elif kitei < dasuu:
-        if dasuu == kitei + 1:
-            result = "ボギー"
-        elif dasuu == kitei + 2:
-            result = "2ボギー"
-        elif dasuu == kitei + 3:
-            result = "3ボギー"
-        elif dasuu == kitei + 4:
-            result = "4ボギー"
-
-    elif kitei > dasuu:
-        if dasuu == kitei - 1:
-            result =  "バーディ"
-        elif dasuu == kitei - 2 and dasuu ==1:
+    if par <= score:
+        if score == 1:
             result = "ホールインワン"
-        elif dasuu == kitei - 2 :
-            result = "イーグル"
-        elif kitei == 5 and dasuu == kitei - 3:
-            result = "アルバトロス"
-        elif kitei == 5 and dasuu == 1:
+        else:
+            result = SCORE_MAPPING[difference]
+    elif par > score:
+        if difference == -4 and par == 5:
             result = "コンドル"
-        elif dasuu == 1:
+        elif score == 1:
             result = "ホールインワン"
+        else:
+            result = SCORE_MAPPING[difference]
 
     return result
-        
 
-# print(len(row1))
 
 for i in range(0,len(row1)):
-    kekka = hantei(row1[i],row2[i])
-    score.append(kekka)
-
-    print(str(row1[i]),str(row2[i]),kekka)
-
-# print(str(score))
+    kekka = hantei(row1[i], row2[i])
+    print(f"ホール {i+1}: par={row1[i]}, score={row2[i]} → {kekka}")
