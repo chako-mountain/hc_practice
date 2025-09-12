13b5ce212e8e1f38d4867499549cb90ced76bb5c
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-09-12 21:38:23 +0900

    簡単な管理者画面まで作成したが、未完成

--- django_ec/product_lists/urls.py
+++ django_ec/product_lists/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 from django.conf.urls import include
-from .views import listsfunction, detailsfunction, adminfunction, showdetailfunction
+from .views import listsfunction, detailsfunction, adminfunction, showdetailfunction, contents_add_function , delete_function, edit_function, update_function
 
 urlpatterns = [
     path('lists' , listsfunction, name='lists' ),
@@ -12,4 +12,8 @@ urlpatterns = [
     # path('detail/int< >' , showdetailfunction , name = 'showdetails')
     # path('detail/<int:id>/', showdetailfunction, name='detail'),
     path('detail/<int:id>/', detailsfunction, name='detail'),
+    path('administrator/submit/', contents_add_function, name = "contents_add"),
+    path('administrator/submit2/', delete_function , name = 'delete' ),
+    path('administrator/submit3/', edit_function , name = 'delete' ),
+    path('administrator/submit3/submit4/', update_function , name = 'delete' )
 ]
--- django_ec/product_lists/views.py
+++ django_ec/product_lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from product_lists.models import ProductList
+from django.http import HttpResponseRedirect 
+
 
 # Create your views here.
 def listsfunction(request):
@@ -32,6 +34,92 @@ def showdetailfunction(request, id):
     info = model.objects.get(id = targetid)
     return render(request, 'details.html', {'info':info})
 
-
-
+# def contents_add_function(request):
+#     print("called")
     
+#     if request.method == "POST":
+    
+#         name = request.POST["name"]
+
+#     return render(request, "contents.html")
+
+
+# 以下は無視してください
+def contents_add_function(request):
+
+
+    if request.method == "POST":
+
+        product_list = ProductList()
+        product_list.name = request.POST["name"]
+        product_list.price = request.POST["price"]
+        product_list.star_rating = request.POST["rating"]
+        product_list.description = request.POST["description"]
+        product_list.is_sale = request.POST.get("is_sale") == "on"
+        product_list.img = request.POST["img"]
+
+
+
+        print("this is name")
+        print(product_list.name)
+
+        product_list.save()
+
+
+
+        print("called")  # 呼び出されたか確認
+        print("Method:", request.method)  # POST かどうか
+        print("POST data:", request.POST)  # 送信データの中身
+
+    return render(request, "contents.html")
+
+
+def delete_function(request):
+    print("called")
+    model = ProductList
+    
+    delete_id = request.POST["post_id"]
+    model.objects.filter(id = delete_id).delete()
+    print("deleted")
+    return render(request, "test.html")
+
+
+def edit_function(request):
+    print("edit is called")
+    model = ProductList
+
+    edit_id = request.POST["edit_id"]
+    print(edit_id)
+    edit_list = model.objects.get(id = edit_id)
+
+    img = edit_list.img
+    print("edit_list")
+    print(edit_list)
+    print(img)
+    return render(request, "edit.html" ,{ "edit_list" : edit_list })
+
+
+def update_function(request):
+
+    product_id = request.POST.get("product_id")
+    update_list = ProductList.objects.get(id=product_id)
+
+     
+
+    update_list.name = request.POST["name"]
+    update_list.price = request.POST["price"]
+    update_list.star_rating = request.POST["rating"]
+    update_list.description = request.POST["description"]
+    update_list.is_sale = request.POST.get("is_sale") == "on"
+    update_list.img = request.POST["img"]
+
+    print("imgですよ")
+    print(update_list.img)
+    update_list.save()
+
+
+
+    return render(request, "administrator.html" )
+
+  
+
