a4b20c52c20abe44ece880e2d1cccb24e630bae1
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-09-21 12:12:03 +0900

    管理者画面完成

--- django_ec/product_lists/urls.py
+++ django_ec/product_lists/urls.py
@@ -5,23 +5,6 @@ from django.views.generic.base import TemplateView
 from django.conf.urls import include
 from .views import product_list_view, product_detail_view, contents_add_function , delete_function, edit_function, update_function,admin_page,admin_product_list_view
 
-# urlpatterns = [
-#     path('lists' , product_list_view, name='lists' ),
-#     path('details' , product_detail_view, name='details' ),
-#     path('administrator/', product_admin_view, name='administrator'),
-#     # path('detail/int< >' , showdetailfunction , name = 'showdetails')
-#     # path('detail/<int:id>/', showdetailfunction, name='detail'),
-#     path('detail/<int:id>/', product_detail_view, name='detail'),
-#     path('administrator/submit/', contents_add_function, name = "contents_add"),
-#     path('administrator/submit2/', delete_function , name = 'delete' ),
-#     path('administrator/submit3/', edit_function , name = 'delete' ),
-#     path('administrator/submit3/submit4/', update_function , name = 'delete' ),
-#     # path('signup', signup_function , name = "signup" ),
-#     # path('login', loginfunction , name = "login"),
-#     path("admin-basic/", admin_page, name="admin_page"),
-# ]
-
-
 urlpatterns = [
     path('lists' , product_list_view, name='lists' ),
     path('details' , product_detail_view, name='details' ),
--- django_ec/product_lists/views.py
+++ django_ec/product_lists/views.py
@@ -11,28 +11,21 @@ from basicauth.decorators import basic_auth_required
 # from django.utils.decorators import method_decorator
 
 
-# class ValidationError(Exception):
-#     print("this is validation error")
-
-# Create your views here.
 def product_list_view(request):
     object_list = ProductList.objects.all()
     return render(request, 'lists.html', {'object_list': object_list})
 
 
 def product_detail_view(request, id):
-    # targetid = id
-    model = ProductList
+
     related_products = ProductList.objects.order_by("-created_at")[:4]
-    # print("リスト表示したい")
-    # print(related_products)
-    product = model.objects.get(id=id)
+    product = ProductList.objects.get(id=id)
     return render(request, 'details.html', {"related_products": related_products, "product": product})
     
-    
+
+@basic_auth_required
 def admin_product_list_view(request):
-    model = ProductList
-    object_list = model.objects.all()
+    object_list = ProductList.objects.all()
     return render(request, 'administrator.html', {'object_list': object_list})
 
 
@@ -40,11 +33,8 @@ def admin_product_list_view(request):
 def contents_add_function(request):
 
     if request.method == "POST":
-    
         product_list = ProductList()
-
-        try: 
-            
+        try:      
             product_list.name = request.POST["name"]
             product_list.price = request.POST["price"]
             product_list.star_rating = request.POST["rating"]
@@ -52,8 +42,7 @@ def contents_add_function(request):
             product_list.is_sale = request.POST.get("is_sale") == "on"
             product_list.img = request.POST["img"]
             product_list.save()
-            return redirect("administrator")
-        
+            return redirect("administrator")       
         except (ValidationError, ValueError):
             print("this is validation error")
             error_message = "PriceまたはStar Ratingは整数で記入してね"
@@ -65,9 +54,8 @@ def contents_add_function(request):
 @basic_auth_required
 def delete_function(request):
     # print("called")
-    model = ProductList
     delete_id = request.POST["post_id"]
-    model.objects.filter(id = delete_id).delete()
+    ProductList.objects.filter(id = delete_id).delete()
     print("deleted")
     # return render(request, "test.html")
     return redirect('administrator')
@@ -75,34 +63,30 @@ def delete_function(request):
 
 @basic_auth_required
 def edit_function(request, id):
-    # print("edit is called")
-    model = ProductList
+    # model = ProductList
     edit_id = request.POST["edit_id"]
-    # print(edit_id)
-    edit_list = model.objects.get(id = edit_id)
+    edit_list = ProductList.objects.get(id=edit_id)
     img = edit_list.img
-    # print("edit_list")
-    # print(edit_list)
-    # print(img)
     return render(request, "edit.html" ,{ "edit_list" : edit_list })
 
 
 @basic_auth_required
-def update_function(request):
+def update_function(request ,id):
 
-    print("cliced")
-
-    product_id = request.POST.get("product_id")
-    update_list = ProductList.objects.get(id=product_id)
-
-
-    update_list.name = request.POST["name"]
-    update_list.price = request.POST["price"]
-    update_list.star_rating = request.POST["rating"]
-    update_list.description = request.POST["description"]
-    update_list.is_sale = request.POST.get("is_sale") == "on"
-    update_list.img = request.POST["img"]
-    update_list.save()
+    try: 
+        product_id = request.POST.get("product_id")
+        update_list = ProductList.objects.get(id=product_id)
+        update_list.name = request.POST["name"]
+        update_list.price = request.POST["price"]
+        update_list.star_rating = request.POST["rating"]
+        update_list.description = request.POST["description"]
+        update_list.is_sale = request.POST.get("is_sale") == "on"
+        update_list.img = request.POST["img"]
+        update_list.save()
+    except (ValidationError, ValueError):
+        print("this is validation error")
+        error_message = "PriceまたはStar Ratingは整数で記入してね"
+        return render(request, "edit.html" ,{ "edit_list" : update_list, "error_message":error_message })
 
     return redirect('administrator')
     # return render(request, "administrator.html" )
