7395c6041cfef95252253748bdda27b14d2abc0d
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-09-25 21:38:29 +0900

    データベースは作成

--- django_ec/product_lists/admin.py
+++ django_ec/product_lists/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ProductList
+# from .models import ProductList
+from .models import ProductList, CartList 
 
 # Register your models here.
 
 admin.site.register(ProductList)
+admin.site.register(CartList)
--- django_ec/product_lists/models.py
+++ django_ec/product_lists/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
+class CartList(models.Model):
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"CartList for user {self.user_id}"
+
+
+
+
 class ProductList(models.Model):
     # id = models.AutoField(primary_key=True)
+    # user_id = models.ForeignKey(CartList,  on_delete=models.PROTECT)
+    user = models.ForeignKey(CartList, on_delete=models.PROTECT)
     name = models.CharField(max_length=255, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     star_rating = models.IntegerField(null=True, blank=True)
@@ -13,8 +25,6 @@ class ProductList(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.name
     
-
