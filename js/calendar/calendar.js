76f065d7d604d9665702847045fa89f838337bef
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-09-29 16:19:43 +0900

    引数を指定して月を表示できた

--- js/calendar/calendar.js
+++ js/calendar/calendar.js
@@ -2,8 +2,8 @@
 
 
 const today = new Date(); // 今日の日付を取得
-const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0); // 当月の翌月の0日を取得
-const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth() , 1);
+let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0); // 当月の翌月の0日を取得
+let firstDayOfMonth = new Date(today.getFullYear(), today.getMonth() , 1);
 
 console.log(lastDayOfMonth.getDate());
 console.log(lastDayOfMonth.getDay());
@@ -11,18 +11,23 @@ console.log(firstDayOfMonth.getDay());
 
 const args = process.argv
 const thirdArg = args[2];
-// console.log("3番目の引数は:", thirdArg);
 
-// if (thirdArg === "-m"){
-//     want_to_month = args[3]
+console.log("3番目の引数は:", thirdArg);
 
-//     lastDayOfMonth = new Date(currentYear, want_to_month-1,0);
-//     first
+if (thirdArg === "-m"){
+
+    // if not (0<=args[3]<=12)
+
+    const currentYear = new Date().getFullYear();
+
+    want_to_month = args[3]
+
+    lastDayOfMonth = new Date(currentYear, want_to_month,0);
+    firstDayOfMonth = new Date(currentYear,want_to_month-1,1);
 
 
 
-
-// }
+}
 
 
 function create_calendar(begin_day,how_many_day) {
@@ -38,7 +43,7 @@ function create_calendar(begin_day,how_many_day) {
 
     process.stdout.write(" ".repeat(beginneg_number) + "1");
 
-    const todayDate = today.getDate();
+
 
     if (week_day_administrator === 7) {
         console.log()
@@ -47,9 +52,6 @@ function create_calendar(begin_day,how_many_day) {
 
 
     for (i= 2 ; i <= how_many_day; i++){
-        // console.log(" ".repeat(beginneg_number));
-        // console.log(i)
-        // console.log(" ".repeat(3))
 
         if (i >= 0 && i < 10) {
             if (week_day_administrator === 6) {
@@ -59,9 +61,6 @@ function create_calendar(begin_day,how_many_day) {
                 
                 console.log()
 
-                
-                // process.stdout.write(" ".repeat(1) + i);
-                // week_day_administrator += 1
             }
             else if(week_day_administrator === 0) {
                 process.stdout.write(" ".repeat(1) + i);
@@ -77,19 +76,7 @@ function create_calendar(begin_day,how_many_day) {
 
         }
         else {
-            // if (week_day_administrator === 6) {
-            //     week_day_administrator = 0
-            //     console.log()
 
-            // }
-
-            // // if(week_day_administrator === 0) {
-            // //     process.stdout.write(i);
-            // //     week_day_administrator += 1
-            // // }
-            
-            // process.stdout.write(" ".repeat(1) + i);
-            // week_day_administrator += 1
 
             if (week_day_administrator === 6) {
                 week_day_administrator = 0
@@ -113,61 +100,9 @@ function create_calendar(begin_day,how_many_day) {
 
         }
 
-       
-
-
-
     }
-    
     
 }
 
-create_calendar(lastDayOfMonth.getDay(),lastDayOfMonth.getDate())
+create_calendar(firstDayOfMonth.getDay(),lastDayOfMonth.getDate())
 
-
-create_calendar(6,30)
-
-// console.log("日 月 火 水 木 金 土")
-// console.log(" ".repeat(7) + "1")
-
-// process.stdout.write(" ".repeat(4) + "1");
-// process.stdout.write('no line break');
-
-
-
-
-
-// const reset = "\x1b[0m";
-// const bgWhite = "\x1b[47m";
-// const fgBlack = "\x1b[30m";
-
-// const todayDate = today.getDate();
-
-// function writeDay(i, spaceCount) {
-//     let dayStr = " ".repeat(spaceCount) + i;
-//     if (i === todayDate) {
-//         dayStr = bgWhite + fgBlack + dayStr + reset;
-//     }
-//     process.stdout.write(dayStr);
-// }
-
-// // ループ内の例
-// if (week_day_administrator === 6) {
-//     week_day_administrator = 0;
-//     console.log();
-//     writeDay(i, 1);  // スペース1つ＋日付出力（背景色判定含む）
-//     week_day_administrator += 1;
-// } else if (week_day_administrator === 0) {
-//     writeDay(i, 1);
-//     week_day_administrator += 1;
-// } else {
-//     writeDay(i, 2);
-//     week_day_administrator += 1;
-// }
-
-// console.log(process.argv);
-
-// // 実際の引数だけを取り出す（先頭2つを除く）
-// const args = process.argv.slice(2);
-
-// console.log("コマンドライン引数:", args);
