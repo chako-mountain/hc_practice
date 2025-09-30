946d58f6e11cc482e9f23d2ddbcee01db72e3b79
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-10-01 07:48:58 +0900

    セーブ

--- django_ec/product_lists/models.py
+++ django_ec/product_lists/models.py
@@ -5,7 +5,7 @@ class BaseModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract = True  # ← これがないと普通のテーブルとして作られてしまう
+        abstract = True 
 
     
 class UserList(BaseModel):
@@ -26,11 +26,46 @@ class ProductList(BaseModel):
     def __str__(self):
         return self.name
     
+    
+
+# class CartList(BaseModel):
+#     user = models.ForeignKey(UserList, on_delete=models.PROTECT)
+#     product = models.ForeignKey(ProductList, on_delete=models.PROTECT)
+#     number = models.IntegerField(null = False, blank = False)
+
+#     def __str__(self):
+#         return str(self.user)
 
 class CartList(BaseModel):
     user = models.ForeignKey(UserList, on_delete=models.PROTECT)
-    product = models.ForeignKey(ProductList, on_delete=models.PROTECT)
-    number = models.IntegerField(null = False, blank = False)
+    FirstName = models.CharField(max_length=255, null=False)
+    LastName = models.CharField(max_length=255, null=False)
+    Username = models.CharField(max_length=255, null=False)
+    Email= email = models.EmailField(max_length=254, unique=True)
+    Address = models.CharField(max_length=255, null=False)
+    Address = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
 
     def __str__(self):
-        return str(self.user)
+        return self.user
+    
+
+class CreditCard(BaseModel):
+    user = models.ForeignKey(CartList, on_delete=models.PROTECT)
+    NameOnCart = models.CharField(max_length=255, null=False)
+    CreditCardNumber = models.IntegerField(null=True, blank=True)
+    Exporation = models.CharField(max_length=255, null=False)
+    CVV = models.CharField(max_length=255, null=False)
+
+
+
+
+
+
+# class CartProduct(models.Model):
+#     user = models.ForeignKey(CartList, on_delete=models.PROTECT)
+#     product = models.ForeignKey(ProductList, on_delete=models.PROTECT)
+
+#     def __str__(self):
+#         return self.user
