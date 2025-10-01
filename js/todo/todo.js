6990bf9ef4ca2d2731e37061cb35ce8f11d34097
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-10-01 17:32:50 +0900

    テキストとボタンを表示

--- js/todo/todo.js
+++ js/todo/todo.js
@@ -22,6 +22,11 @@
 // }
 
 // let nameText = document.getElementById("contents");
+
+
+
+
+
 let msg = document.getElementById("msg");
 
 let checkButton = document.getElementById('checkButton');
@@ -38,37 +43,73 @@ function create_todo_lists() {
     lists.setAttribute("id",index);
     
 
-    document.body.appendChild(lists);
+    // document.body.appendChild(lists);
     
-    contents = document.getElementById(index);
-    let nameText = document.getElementById("contents");
-    contents.innerText = nameText.value
+    // contents = document.getElementById(index);
+    // let nameText = document.getElementById("contents");
+    // contents.innerText = nameText.value;
     
-    const hr = document.createElement("hr");
-    lists.appendChild(hr);
+    let nametext = document.getElementById("contents");
+    const textSpan = document.createElement("span");
+    textSpan.innerText = nametext.value;
+    lists.appendChild(textSpan)
 
     
 
     const delete_button = document.createElement("button")
-    document.body.appendChild(delete_button);
-    delete_button.value = "削除";
+    // delete_button.value = "削除";
+    delete_button.innerText = "削除"
     delete_button.setAttribute("id","delete");
+    lists.appendChild(delete_button); 
+
 
     const edit_button = document.createElement("button")
-    document.body.appendChild(edit_button);
-    edit_button.value = "削除";
+    // edit_button.value = "編集";
+    edit_button.innerText = "編集"
     edit_button.setAttribute("id","edit");
+    lists.appendChild(edit_button);
 
 
-    
 
-    
+    const hr = document.createElement("hr");
+    lists.appendChild(hr);
 
 
-    
+    document.body.appendChild(lists);
+
+    // contents = document.getElementById(index);
+    // let nameText = document.getElementById("contents");
+    // contents.innerText = nameText.value;
 
     
 
 }
 
 
+// function create_todo_lists() {
+    
+//     index +=1;
+    
+//     let lists = document.createElement("div");
+//     lists.setAttribute("id", index);
+    
+//     let nameText = document.getElementById("contents");
+//     lists.innerText = nameText.value;
+    
+//     // ボタンをdivの中に追加
+//     const delete_button = document.createElement("button");
+//     delete_button.innerText = "削除";
+//     delete_button.classList.add("delete-button");  // classに変更
+    
+//     const edit_button = document.createElement("button");
+//     edit_button.innerText = "編集";
+//     edit_button.classList.add("edit-button");  // classに変更
+    
+//     lists.appendChild(delete_button);
+//     lists.appendChild(edit_button);
+    
+//     const hr = document.createElement("hr");
+//     lists.appendChild(hr);
+    
+//     document.body.appendChild(lists);
+// }
